893038e46eff1860946be814138b9137cef1a041
Student-Prabin <dahalprabin74@gmail.com>
2024-07-10 11:51:05 -0700

    ch

--- src/App.js
+++ src/App.js
@@ -3,34 +3,41 @@ import './index.css';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import About from './pages/About';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Page1 from './pages/sub-pages/Page1';
 import Page2 from './pages/sub-pages/Page2';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import RootLayout from './components/RootLayout';
 
 const App = () => {
 
     const router = createBrowserRouter([
       {
         path:'/',
-        element:<Home />,
+        element:<RootLayout />,
         children:[
           {
             index:true,
+            element:<Home />
+          },
+          {
+            path:'page1',
             element:<Page1 />
-          },{
+          },
+          {
             path:'page2',
             element:<Page2 />
+          },
+          {
+            path:'contact-page',
+            element:<Contact />
+          },{
+            path:'about-page',
+            element:<About />
           }
           
         ]
       },
-      {
-        path:'contact-page',
-        element:<Contact />
-      },{
-        path:'about-page',
-        element:<About />
-      }
+      
     ]);
 
     return <RouterProvider router={router} />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,9 +5,9 @@ import { NavLink, Outlet } from 'react-router-dom'
 const Home = () => {
   return (
     <div>
-      <Header />
+      
       <h1>I am Home</h1>
-      <NavLink to={'/'}>Page 1</NavLink>
+      <NavLink to={'page1'}>Page 1</NavLink>
       <NavLink to={'/page2'}>Page 2</NavLink>
       <Outlet />
     </div>
